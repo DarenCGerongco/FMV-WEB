b45860e46287cadca49805c996fe2f1d8df92c09
worthypeople <gregjanabad@gmail.com>
2024-09-28 18:34:23 +0800

    update delivery
    hehehe yey

--- src/pages/delivery.jsx
+++ src/pages/delivery.jsx
@@ -33,7 +33,7 @@ function Delivery() {
     <div className="flex w-full bg-gray-100">
       <Navbar />
       <div className="w-full ml-80 bg-white">
-        <div className="w-4/5 mx-auto bg-white p-6 m-3 rounded-lg shadow-md mb-6">
+        <div className="w-4/5 mx-auto bg-white p-6 m-3 rounded-lg shadow-2xl mb-6 border">
           <h2 className="text-1xl font-bold">Management System Delivery</h2>
         </div>
         <div className="w-4/5 mx-auto bg-white p-5 m-3 rounded-lg shadow-xl">
@@ -46,26 +46,26 @@ function Delivery() {
                 className="flex-grow focus:outline-none px-4 py-2 rounded-md shadow-sm sm:text-sm border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full"
                 placeholder="Search for Delivery man"
               />
-              <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded-md shadow-md focus:outline-none">
+              <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded-md shadow-2xl focus:outline-none">
                 Search
               </button>
             </div>
           </div>
 
           {/* 1st Container - Confirmation */}
-          <h2 className="text-xs mt-10 text-green-600 font-bold">Confirmation</h2>
-          <div className="mt-2 bg-gray-100 p-4 rounded-lg shadow-md">
-            <h3 className="text-sm text-gray-400 flex justify-between whitespace-nowrap">
-              <span className="text-sm ml-1 text-gray-400 w-1/5">Delivery NO.</span>
-              <span className="text-sm text-gray-400 mr-36">Delivered to</span>
-              <span className="text-sm text-gray-400 mr-36">Delivery Man</span>
-              <span className="text-sm text-gray-400 mr-36">Date</span>
+          <h2 className="text-xs mt-10 text-green-600 font-bold ">Confirmation</h2>
+          <div className="mt-2 bg-gray-100 p-4 rounded-lg shadow-2xl">
+            <h3 className="text-sm text-gray-400 flex justify-between items-center">
+              <span className="w-1/6 text-left">Delivery Number.</span>
+              <span className="w-1/3 text-left">Delivered to</span>
+              <span className="w-1/3 text-left">Delivery Man</span>
+              <span className="w-1/6 text-left">Date</span>
             </h3>
-            <div className="bg-white p-4 rounded-lg shadow-md flex justify-between items-center">
-              <span>2</span>
-              <span>Barangay Lumbia</span>
-              <span>Arlene Abad</span>
-              <span>06/04/2024</span>
+            <div className="bg-white p-4 rounded-lg shadow-2xl flex justify-between items-center bg-[#8EF7A8]">
+              <span className="w-1/6">2</span>
+              <span className="w-1/3">Barangay Lumbia</span>
+              <span className="w-1/3">Arlene Abad</span>
+              <span className="w-1/6">06/04/2024</span>
               <img
                 src="./src/assets/info.png"
                 alt="Delivery Image"
@@ -73,69 +73,57 @@ function Delivery() {
                 onClick={() => handleOpenConfirmationModal({ deliveryNo: 2, deliveredTo: 'Barangay Lumbia', deliveryMan: 'Arlene Abad', date: '06/04/2024' })}
               />
             </div>    
-            <div className="bg-white mt-3 p-4 rounded-lg shadow-md flex justify-between items-center">
-              <span>2</span>
-              <span>Barangay Lumbia</span>
-              <span>Arlene Abad</span>
-              <span>06/04/2024</span>
+            <div className="bg-white mt-3 p-4 rounded-lg shadow-2xl flex justify-between items-center bg-[#8EF7A8]">
+              <span className="w-1/6">3</span>
+              <span className="w-1/3">Barangay Iponan</span>
+              <span className="w-1/3">John Smith</span>
+              <span className="w-1/6">06/05/2024</span>
               <img
                 src="./src/assets/info.png"
                 alt="Delivery Image"
                 className="w-7 h-7 rounded-full cursor-pointer"
-                onClick={() => handleOpenConfirmationModal({ deliveryNo: 2, deliveredTo: 'Barangay Lumbia', deliveryMan: 'Arlene Abad', date: '06/04/2024' })}
+                onClick={() => handleOpenConfirmationModal({ deliveryNo: 3, deliveredTo: 'Barangay Iponan', deliveryMan: 'John Smith', date: '06/05/2024' })}
               />
             </div>        
           </div>
 
           {/* 2nd Container - Ongoing Delivery */}
           <h2 className="text-xs mt-10 text-green-600 font-bold">Ongoing Delivery</h2>
-          <div className="mt-2 bg-gray-100 p-4 rounded-lg shadow-md">
-            <h3 className="text-sm text-gray-400 flex justify-between whitespace-nowrap">
-              <span className="text-sm ml-1 text-gray-400 w-1/5">Delivery NO.</span>
-              <span className="text-sm text-gray-400 mr-36">Delivered to</span>
-              <span className="text-sm text-gray-400 mr-36">Delivery Man</span>
-              <span className="text-sm text-gray-400 mr-36">Date</span>
+          <div className="mt-2 bg-gray-100 p-4 rounded-lg shadow-2xl">
+            <h3 className="text-sm text-gray-400 flex justify-between items-center">
+              <span className="w-1/6 text-left">Delivery Number.</span>
+              <span className="w-1/3 text-left">Delivered to</span>
+              <span className="w-1/3 text-left">Delivery Man</span>
+              <span className="w-1/6 text-left">Date</span>
             </h3>
-            <div className="bg-white p-4 rounded-lg shadow-md flex justify-between items-center">
-              <span>3</span>
-              <span>Iponan</span>
-              <span>Edelcris Cabarrubias</span>
-              <span>07/04/2024</span>
+            <div className="bg-white p-4 rounded-lg shadow-2xl flex justify-between items-center bg-[#E6FCE6]">
+              <span className="w-1/6">4</span>
+              <span className="w-1/3">Iponan</span>
+              <span className="w-1/3">Edelcris Cabarrubias</span>
+              <span className="w-1/6">07/04/2024</span>
               <img
                 src="./src/assets/info.png"
                 alt="Delivery Image"
                 className="w-7 h-7 rounded-full cursor-pointer"
-                onClick={() => handleOpenOngoingModal({ deliveryNo: 3, deliveredTo: 'Iponan', deliveryMan: 'Edelcris Cabarrubias', date: '07/04/2024' })}
+                onClick={() => handleOpenOngoingModal({ deliveryNo: 4, deliveredTo: 'Iponan', deliveryMan: 'Edelcris Cabarrubias', date: '07/04/2024' })}
               />
             </div>
           </div>
 
-          {/* 3rd Container - Order Delivered */}
-          <h2 className="text-xs mt-10 text-green-600 font-bold">Order Delivered</h2>
-          <div className="mt-2 bg-gray-100 p-4 rounded-lg shadow-md">
-            <h3 className="text-sm text-gray-400 flex justify-between whitespace-nowrap">
-              <span className="text-sm ml-1 text-gray-400 w-1/5">Delivery NO.</span>
-              <span className="text-sm text-gray-400 mr-36">Delivered to</span>
-              <span className="text-sm text-gray-400 mr-36">Delivery Man</span>
-              <span className="text-sm text-gray-400 mr-36">Date</span>
+          {/* 3rd Container - Delivered Order */}
+          <h2 className="text-xs mt-10 text-gray-400 font-bold">Delivered Order</h2>
+          <div className="mt-2 bg-gray-100 p-4 rounded-lg shadow-2xl">
+            <h3 className="text-sm text-gray-400 flex justify-between items-center">
+              <span className="w-1/6 text-left">Delivery Number.</span>
+              <span className="w-1/3 text-left">Delivered to</span>
+              <span className="w-1/3 text-left">Delivery Man</span>
+              <span className="w-1/6 text-left">Date</span>
             </h3>
-            <div className="bg-white p-4 rounded-lg shadow-md flex justify-between items-center">
-              <span>5</span>
-              <span>Barangay Talaga</span>
-              <span>Mark Cabarrubias</span>
-              <span>09/04/2024</span>
-              <img
-                src="./src/assets/info.png"
-                alt="Delivery Image"
-                className="w-7 h-7 rounded-full cursor-pointer"
-                onClick={() => handleOpenOrderDeliveredModal({ deliveryNo: 5, deliveredTo: 'Barangay Talaga', deliveryMan: 'Mark Cabarrubias', date: '09/04/2024' })}
-              />
-            </div>
-            <div className="bg-white p-4 rounded-lg shadow-md flex justify-between items-center mt-3">
-              <span>5</span>
-              <span>Barangay Talaga</span>
-              <span>Mark Cabarrubias</span>
-              <span>09/04/2024</span>
+            <div className="bg-white p-4 rounded-lg shadow-2xl flex justify-between items-center bg-[#8EF7A8]">
+              <span className="w-1/6">5</span>
+              <span className="w-1/3">Barangay Talaga</span>
+              <span className="w-1/3">Mark Cabarrubias</span>
+              <span className="w-1/6">09/04/2024</span>
               <img
                 src="./src/assets/info.png"
                 alt="Delivery Image"
@@ -150,20 +138,20 @@ function Delivery() {
             <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
               <div className="bg-white p-6 rounded-lg shadow-lg w-1/3 max-h-screen overflow-y-auto">
                 <h2 className="text-xl font-bold mb-4">Order Delivered Details</h2>
-                <p><strong>Delivery NO:</strong> {selectedDelivery.deliveryNo}</p>
+                <p><strong>Delivery Number:</strong> {selectedDelivery.deliveryNo}</p>
                 <p><strong>Delivered to:</strong> {selectedDelivery.deliveredTo}</p>
                 <p><strong>Delivery Man:</strong> {selectedDelivery.deliveryMan}</p>
                 <p><strong>Date:</strong> {selectedDelivery.date}</p>
 
                 {/* ongoing delivered modal content */}
-                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-2xl">
                   <div className="flex justify-between">
                     <span>₱1500</span>
                     <span>Submersible Pump</span>
                     <span>x5</span>
                   </div>
                 </div>
-                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-2xl">
                   <div className="flex justify-between">
                     <span>₱1500</span>
                     <span>Submersible Pump</span>
@@ -175,7 +163,7 @@ function Delivery() {
                   <img
                     src="./src/assets/darwen.png"
                     alt="Proof of Delivery"
-                    className="w-auto h-auto rounded-lg shadow-md"
+                    className="w-auto h-auto rounded-lg shadow-2xl"
                   />
                 </div>
                 <div className="mt-4">
@@ -184,7 +172,7 @@ function Delivery() {
                 </div>
 
                 <button
-                  className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-md focus:outline-none"
+                  className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-2xl focus:outline-none"
                   onClick={handleCloseModal}
                 >
                   Close
@@ -198,20 +186,20 @@ function Delivery() {
             <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
               <div className="bg-white p-6 rounded-lg shadow-lg w-1/3 max-h-screen overflow-y-auto">
                 <h2 className="text-xl font-bold mb-4">Confirmation Details</h2>
-                <p><strong>Delivery NO:</strong> {selectedDelivery.deliveryNo}</p>
+                <p><strong>Delivery Number:</strong> {selectedDelivery.deliveryNo}</p>
                 <p><strong>Delivered to:</strong> {selectedDelivery.deliveredTo}</p>
                 <p><strong>Delivery Man:</strong> {selectedDelivery.deliveryMan}</p>
                 <p><strong>Date:</strong> {selectedDelivery.date}</p>
 
                 {/* Content for confirm delivery */}
-                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-2xl">
                   <div className="flex justify-between">
                     <span>₱1500</span>
                     <span>Submersible Pump</span>
                     <span>x5</span>
                   </div>
                 </div>
-                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-2xl">
                   <div className="flex justify-between">
                     <span>₱1500</span>
                     <span>Submersible Pump</span>
@@ -224,7 +212,7 @@ function Delivery() {
                   <img
                     src="./src/assets/darwen.png" 
                     alt="Proof of Delivery"
-                    className="w-auto h-auto rounded-lg shadow-md"
+                    className="w-auto h-auto rounded-lg shadow-2xl"
                   />
                 </div>
 
@@ -234,7 +222,7 @@ function Delivery() {
                 </div>
 
                 <button
-                  className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-md focus:outline-none"
+                  className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-2xl focus:outline-none"
                   onClick={handleCloseModal}
                 >
                   Close
@@ -248,14 +236,14 @@ function Delivery() {
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
             <div className="bg-white p-6 rounded-lg shadow-lg w-1/3 max-h-screen overflow-y-auto">
             <h2 className="text-xl font-bold mb-4">Ongoing Delivery Details</h2>
-            <p><strong>Delivery NO:</strong> {selectedDelivery.deliveryNo}</p>
+            <p><strong>Delivery Number:</strong> {selectedDelivery.deliveryNo}</p>
             <p><strong>Delivered to:</strong> {selectedDelivery.deliveredTo}</p>
             <p><strong>Delivery Man:</strong> {selectedDelivery.deliveryMan}</p>
             <p><strong>Date:</strong> {selectedDelivery.date}</p>
 
             <div className="mt-4">
                 <h3 className="text-lg font-semibold mb-2">Order List:</h3>
-                <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+                <div className="p-4 bg-gray-100 rounded-lg shadow-2xl">
                 <div className="flex justify-between">
                     <span>₱1500</span>
                     <span>Submersible Pump</span>
@@ -265,7 +253,7 @@ function Delivery() {
             </div>
 
         <div className="mt-4">
-        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <div className="p-4 bg-gray-100 rounded-lg shadow-2xl">
           <div className="flex justify-between">
             <span>₱2500</span>
             <span>Water Heater</span>
@@ -275,7 +263,7 @@ function Delivery() {
          </div>
 
          <button
-        className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-md focus:outline-none"
+        className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-2xl focus:outline-none"
         onClick={handleCloseModal} >
         Close
          </button>
@@ -288,20 +276,20 @@ function Delivery() {
             <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
               <div className="bg-white p-6 rounded-lg shadow-lg w-1/3 max-h-screen overflow-y-auto">
                 <h2 className="text-xl font-bold mb-4">Order Delivered Details</h2>
-                <p><strong>Delivery NO:</strong> {selectedDelivery.deliveryNo}</p>
+                <p><strong>Delivery Number:</strong> {selectedDelivery.deliveryNo}</p>
                 <p><strong>Delivered to:</strong> {selectedDelivery.deliveredTo}</p>
                 <p><strong>Delivery Man:</strong> {selectedDelivery.deliveryMan}</p>
                 <p><strong>Date:</strong> {selectedDelivery.date}</p>
 
                 {/* Example content */}
-                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-2xl">
                   <div className="flex justify-between">
                     <span>₱1500</span>
                     <span>Submersible Pump</span>
                     <span>x5</span>
                   </div>
                 </div>
-                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-md">
+                <div className="mt-4 p-4 bg-gray-100 rounded-lg shadow-2xl">
                   <div className="flex justify-between">
                     <span>₱1500</span>
                     <span>Submersible Pump</span>
@@ -314,7 +302,7 @@ function Delivery() {
                   <img
                     src="./src/assets/darwen.png"
                     alt="Proof of Delivery"
-                    className="w-auto h-auto rounded-lg shadow-md"
+                    className="w-auto h-auto rounded-lg shadow-2xl"
                   />
                 </div>
 
@@ -324,7 +312,7 @@ function Delivery() {
                 </div>
 
                 <button
-                  className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-md focus:outline-none"
+                  className="mt-4 bg-blue-500 text-white ml-80 px-4 py-2 rounded-md shadow-2xl focus:outline-none"
                   onClick={handleCloseModal}
                 >
                   Close
