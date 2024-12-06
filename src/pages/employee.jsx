13638a7868dbb8f1c4fc151736bf7566afa0ccdc
worthypeople <gregjanabad@gmail.com>
2024-12-07 02:14:02 +0800

    cha
    cha

--- src/pages/employee.jsx
+++ src/pages/employee.jsx
@@ -205,7 +205,8 @@ function Employee() {
               />
             </div>
             <button
-              className="flex bg-blue-500 text-white w-[10rem] duration-200 justify-center hover:text-blue-500 hover:bg-white items-center font-bold shadow-md rounded"
+              className="flex bg-blue-500 text-white w-[10rem] duration-200 justify-center rounded hover:bg-blue-700 rounded-lg items-center font-bold"
+              // bg-blue-500 text-white rounded hover:bg-blue-700 rounded-lg
               onClick={openAddModal}
             >
               Add Employee
